c36a55b251df5e18f55bd7648379c01c900e60a0
A01798012 <a01798012@tec.mx>
2023-04-17 09:15:43 -0600

    Se agrego la funcion para insertar tutor y usuario

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const sql = require("mssql");
 const app = express();
 app.use(express.json());
 // TUTOR
 //insert new tutor
-app.post('/api/tutor', (req, res)=>{
+
+const sqlConfig = {
+    user: 'sa',
+    password: process.env.db_pswd,
+    server: 'localhost',
+    database: 'JuegoCrackTheCode',
+    port: 1433,
+    options:{
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+//insert tutor
+app.post('/api/tutors', async function (req, res) {
     const tutor = {
         nombreTutor: req.body.nombreTutor,
         primerApellido: req.body.primerApellido,
@@ -12,28 +26,52 @@ app.post('/api/tutor', (req, res)=>{
         telefono: req.body.telefono,
         codTel: req.body.codTel
     };
-    res.send(tutor);
+    let pool = await sql.connect(sqlConfig);
+    let request = pool.request();
+//    request.input(tutor.nombreTutor, tutor.primerApellido, tutor.email, tutor.password, tutor.telefono, tutor.codTel);
+    request.input('NombreTutor', tutor.nombreTutor);
+    request.input('PrimerApellido', tutor.primerApellido);
+    request.input('Email', tutor.email);
+    request.input('Password', tutor.password);
+    request.input('Telefono', sql.Int, tutor.telefono);
+    request.input('CodTel', sql.Int , tutor.codTel);
+    let result = await request.execute('PROC_Insertar_Tutor');
+    res.send(result);
 });
 //alter password
+app.put('/api/tutors/passwords', (req, res)=>{
+    const password ={
+        password: req.body.password
+    };
+    res.send(password.password);
+});
+
 //:
 
 // USER
 //insert new user
-app.post('/api/user', (req, res)=>{
+app.post('/api/users', async function(req, res){
     const user = {
         nombreUsuario: req.body.nombreUsuario,
         primerApellido: req.body.primerApellido,
         gamertag: req.body.gamertag,
         password: req.body.password,
-        calificacion: req.body.calificacion,
-        edad: req.body.edad,
-        iDTutor: req.body.iDTutor, // TODO ???
-        codTel: req.body.codTel
+        fecha: req.body.fecha,
+        emailTutor: req.body.emailTutor
     };
-    res.send(user);
+    let pool = await sql.connect(sqlConfig);
+    let request = pool.request();
+    request.input('NombreUsuario', user.nombreUsuario);
+    request.input('PrimerApellido', user.primerApellido);
+    request.input('GamerTag', user.gamertag);
+    request.input('Password', user.password);
+    request.input('Fecha', user.fecha);
+    request.input('EmailTutor', user.emailTutor);
+    let result = await request.execute('PROC_Insertar_Usuario');
+    res.send(result);
 });
 //insert comment
-app.post('/api/comment', (req, res)=>{
+app.post('/api/users/comments', (req, res)=>{
     const comment = {
         comentario: req.body.comentario,
         idUsuario: req.body.idUsuario //TODO ???
@@ -41,18 +79,52 @@ app.post('/api/comment', (req, res)=>{
     res.send(comment);
 });
 //validate password
-//alter calificacion
-//alter gamertag
-//alter password
+app.post('/api/users/passwords', (req, res)=>{
+    const password = {
+        password: req.body.password
+    };
+    res.send(password);
+});
 
+//alter calificacion
+app.put('/api/users/scores', (req, res)=>{
+    const scores = {
+        scores: req.body.scores
+    };
+    res.send(scores);
+});
+//alter gamertag
+app.put('/api/users/gamertags', (req, res)=>{
+    const gamertag = {
+        gamertag: req.body.gamertag
+    }
+    res.send(gamertag);
+});
+//alter password
+app.put('/api/users/passwords', (req, res)=>{
+    const password = {
+        user: req.body.user,
+        previousPassword: req.body.previousPassword,
+        password: req.body.password
+    };
+    res.send(password);
+});
 //Partida
 //alter score
-app.put('/api/score', (req, res)=>{
-
+app.put('/api/users/scores', (req, res)=>{
+    const scores = {
+        user: req.body.user,
+        score: req.body.score
+    };
+    res.send(scores);
 });
 //alter actual progresss
 app.put('/api/progress', (req, res)=>{
-
+    const progress = {
+        user: req.body.user,
+        progress: req.body.progress
+    };
+    res.send(progress);
 });
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
